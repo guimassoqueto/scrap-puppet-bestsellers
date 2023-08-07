daba1d7898069f78d50eb06fbe32d17280f9daf3
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-07 14:31:56 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("oi")
+import { AMAZON_BESTSELLING_URLS } from "./settings.js"
+import { BestSellingAmazon } from "./service/best-selling-amazon.js"
+import { extractPidFromUrl } from "./helpers/extract-pid-from-url.js"
+import { RabbitMQPublisher } from "./rabbitmq/publisher.js"
+
+
+try {
+  const itemsURL = await BestSellingAmazon.get(AMAZON_BESTSELLING_URLS)
+  const pidList = extractPidFromUrl(itemsURL)
+  await RabbitMQPublisher.publish({ "amazon-colly": pidList })
+  console.log(`[x] ${pidList.length} items sent`)
+} catch (error) {
+  console.error(error)
+}
